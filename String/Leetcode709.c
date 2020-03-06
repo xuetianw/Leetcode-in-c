c303c26174514a3edd447f1e834349daee2c7e46
xuetianw <xuetianw@sfu.ca>
2020-03-06 05:16:21 -0800

    709

---
+++ String/Leetcode709.c
@@ -0,0 +1,28 @@
+//
+// Created by fred on 2020-03-06.
+//
+#include "string.h"
+#include "stdlib.h"
+#include <stdio.h>
+
+char *toLowerCase(char * str){
+    unsigned int len = strlen(str);
+    char* lower_case_arr = malloc((len + 1) * sizeof(char));
+    for (int i = 0; i < len; i++) {
+        char el = str[i];
+        if (el >= 'A' && el <= 'Z') {
+            lower_case_arr[i] = el - ('A' - 'a');
+        } else {
+            lower_case_arr[i] = el;
+        }
+    }
+    lower_case_arr[len] = '\0';
+    return lower_case_arr;
+}
+
+int main(int argc, char ** argv) {
+    char* arr = toLowerCase("As");
+    printf("length of arr is %lu\n", strlen(arr));
+    printf("the arra is %s\n", arr);
+    free(arr);
+}
