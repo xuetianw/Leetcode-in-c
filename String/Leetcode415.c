2914dbbdaae140c93416a1046b06b3382ca1dbf3
xuetianw <xuetianw@sfu.ca>
2020-03-08 03:20:01 -0700

    fix 415

--- String/Leetcode415.c
+++ String/Leetcode415.c
@@ -5,24 +5,31 @@
 #include "stdlib.h"
 #include <stdio.h>
 
+void reverse_string(int len, char* res_char);
+
 char* addStrings(char* num1, char* num2) {
     int length1 = strlen(num1);
     int length2 = strlen(num2);
-    int len = length1 > length2 ? length1 : length2;
-    char* res_char = malloc((len + 1) * sizeof(char));
+    int len = 0;
+    char* res_char = NULL;
 
     int carry = 0;
     int i = length1 - 1;
     int j = length2 - 1;
     while (i >= 0 || j >= 0) {
 
-        int digit1 = (j >= 0) ? num1[i] - '0' : 0;
+        int digit1 = (i >= 0) ? num1[i] - '0' : 0;
         int digit2 = (j >= 0) ? num2[j] - '0' : 0;
 
         int sum = digit1 + digit2 + carry;
         char temp[2];
         sprintf(temp, "%d", sum % 10);
-        strcat(res_char, temp);
+
+        len++;
+        char* new_ptr = realloc(res_char, (len + 1) * sizeof(char));
+        new_ptr[len - 1] = temp[0];
+        new_ptr[len] = temp[1];
+        res_char = new_ptr;
 //        printf("res_char len : %lu\n", strlen(res_char));
         carry = sum / 10;
         i--;
@@ -30,14 +37,27 @@ char* addStrings(char* num1, char* num2) {
     }
 
     if (carry != 0) {
-        res_char = realloc(res_char, ((len + 2)) * sizeof(char));
-        sprintf(res_char, "%d", 1);
+        len++;
+        char* new_ptr = realloc(res_char, (len + 1) * sizeof(char));
+        new_ptr[len - 1] = '1';
+        new_ptr[len] = '\0';
+        res_char = new_ptr;
     }
+    reverse_string(len, res_char);
+
     return res_char;
 }
 
+void reverse_string(int len, char* res_char) {// reverse string
+    for (int i = 0; i < len / 2; i++) {
+        char temp = res_char[i];
+        res_char[i] = res_char[len - i - 1];
+        res_char[len - i - 1] = temp;
+    }
+}
+
 int main(int argc, char** argv) {
-    char* arr = addStrings("123", "456");
+    char* arr = addStrings("9", "99");
     printf("%lu\n", strlen(arr));
     free(arr);
 //    char str1[12] = "Hello";
