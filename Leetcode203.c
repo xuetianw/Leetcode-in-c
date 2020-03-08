a420c378b999b72f202af0ee7b93af5fac5efe01
xuetianw <xuetianw@sfu.ca>
2020-03-07 23:15:29 -0800

    203

--- Leetcode203.c
+++ Leetcode203.c
@@ -8,6 +8,27 @@ struct ListNode {
     struct ListNode* next;
 };
 
+//todo
 struct ListNode* removeElements(struct ListNode* head, int val){
+    struct ListNode* cur = head;
+    while (cur != NULL && cur->val == val) {
+        cur = cur->next;
+    }
+    head = cur;
+    if (cur == NULL) {
+        return NULL;
+    }
+    struct ListNode* prev = cur;
+    cur = cur->next;
+    while (cur!= NULL) {
+        if (cur->val == val) {
+            prev->next = cur->next;
+            cur = cur->next;
+        } else {
+            prev = cur;
+            cur = cur->next;
+        }
 
+    }
+    return head;
 }
